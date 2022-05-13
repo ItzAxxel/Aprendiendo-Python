6e93071ca43c517883cdf4ab14c056f5916434d1
ItzAxxel <axel.arroyo.gimenez@gmail.com>
2022-05-14 01:10:53 +0200

    Tipos de variables

---
+++ variables.py
@@ -0,0 +1,19 @@
+#¡Aquí practicaré el uso de las variables y su asignación!
+#Python detecta automaticamente que tipo de variable uso.
+
+#Se puede asignar de esta forma: 
+numeroUno = 1 #Número entero (int)
+numeroDos = 2.4 #Número decimal (float)
+texto = "¡Hola!" #String (texto)
+verdadero = True #Boolean (True)
+falso = False #Boolean (False)
+
+# TODO Investigar acerca de otro tipo de variables pertenecientes a Python; Conjuntos, listas, tupulas y diccionarios
+
+#O de esta otra: 
+numeroTres, numeroCuatro, chat, verdadero2, falso2 = 2, 4.2, "¡Adiós!", True, False
+
+#Printo las variables para ver que todo está correcto.
+print(numeroUno, " " , numeroDos, " " ,texto, " " , verdadero , " ", falso)
+print()
+print(numeroTres, " " ,numeroCuatro, " " ,chat, " ", verdadero2, " " , falso2)
